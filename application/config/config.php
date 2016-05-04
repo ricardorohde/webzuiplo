7b852767ad525ed056170f8dc33c49d6f263480e
amandaazallia <mandazal123@gmail.com>
2016-05-04 14:22:26 +0700

    update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/webzuiplo/';
+$config['base_url'] = 'http://103.27.206.159/zuiplo/';
 
 /*
 |--------------------------------------------------------------------------
