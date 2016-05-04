c29fa0ee52e9208b4cc51dc483efb94b85d394e6
amandaazallia <mandazal123@gmail.com>
2016-05-04 23:00:54 +0700

    smint

--- assets/bootstrap/dist/js/smint.js
+++ assets/bootstrap/dist/js/smint.js
@@ -1,51 +1,7 @@
-// Scroll page with easing effect
-    $('.join  a[href*=#]').bind('click', function(e) {
-        e.preventDefault();
-        target = this.hash;
-        $.scrollTo(target, 1500, {
-            easing: 'easeOutCubic'
-        });
+$('.subMenu').smint({
+	'scrollSpeed' : 1000
+});
 
-        $(".btn-default .button-cover").click();
-    });
-
-        // Scroll page with easing effect
-    $('#footerMenu a[href*=#]').bind('click', function(e) {
-        e.preventDefault();
-        target = this.hash;
-        $.scrollTo(target, 1500, {
-            easing: 'easeOutCubic'
-        });
-
-        $(".btn-navbar").click();
-    });
-
-
-
-    // Show/Hide Sticky "Go top" button
-    $(window).scroll(function(){
-        if($(this).scrollTop()>200){
-            $(".go-top").fadeIn(200);
-        }
-        else{
-            $(".go-top").fadeOut(200);
-        }
-    });
-
-    // Scroll Page to Top when clicked on "go top" button
-    $(".brand, .go-top").click(function(event){
-        event.preventDefault();
-
-        $.scrollTo('#carouselSection', 1500, {
-            easing: 'easeOutCubic'
-        });
-    });
-
-
-
-$(function(){
-
-
-
-
+$(document).ready( function() {
+	$('.subMenu').smint();
 });
