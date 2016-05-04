ad5f1ec75d2eef14ed712cdb4f0e1c4ae411e76f
amandaazallia <mandazal123@gmail.com>
2016-05-04 22:12:37 +0700

    update

--- application/views/dashboard/welcome.php
+++ application/views/dashboard/welcome.php
@@ -87,7 +87,7 @@
   .site-wrapper {
     display: table;
     width: 100%;
-    height: 400px;
+    height: 900px;
     min-height: 100%;
     -webkit-box-shadow: inset 0 0 100px rgba(0,0,0,.5);
     box-shadow: inset 0 0 100px rgba(0,0,0,.5);
