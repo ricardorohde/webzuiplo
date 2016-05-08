ccb8f4d71d519758c100986e8c288676516a68f8
amandaazallia <mandazal123@gmail.com>
2016-05-08 18:09:41 +0700

    update

--- application/views/find/pemilik/find.php
+++ application/views/find/pemilik/find.php
@@ -34,21 +34,8 @@
         </div>
     </div>
 
-    <div id="main-wrapper">
-        <div id="main">
-            <div id="main-inner"><div class="breadcrumb-wrapper">
-    <div class="container">
-        <ol class="breadcrumb pull-left">
 
-            <li class="active">Register</li>
-            <li><a href="#">Vivamus</a></li>
-            <li><a href="#">Proin era</a></li>
-        </ol>
-
-    </div>
-</div>
-
-    <!-- <div id="map">
+    <div id="map">
 
     </div>
     <div id="main-wrapper">
@@ -72,7 +59,93 @@
         </ol>
 
     </div>
-</div>  -->
+</div>
+
+        <script>
+      function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 10,
+          center: {lat: -6.204831, lng: 106.840848}
+        });
+
+        var geocoder = new google.maps.Geocoder();
+
+        document.getElementById('submit').addEventListener('click', function() {
+          geocodeAddress(geocoder, map);
+        });
+      }
+
+      function geocodeAddress(geocoder, resultsMap) {
+        var address = document.getElementById('address').value;
+        geocoder.geocode({'address': address}, function(results, status) {
+          if (status === google.maps.GeocoderStatus.OK) {
+            resultsMap.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+              map: resultsMap,
+              position: results[0].geometry.location
+            });
+            var lat = results[0].geometry.location.lat();
+            var lng = results[0].geometry.location.lng();
+          } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+          }
+          document.getElementById('lat').value = lat;
+          document.getElementById('lng').value=lng;
+        });
+      }
+      var marker = null;
+    function initialize() {
+      var mapOptions = {
+        center: new google.maps.LatLng(-6.175486, 106.825407),
+        zoom: 15
+      };
+      var map = new google.maps.Map(document.getElementById("map-canvas"),
+          mapOptions);
+      google.maps.event.addListener(map, 'click', function(event) {
+        if (marker != undefined){
+          marker.setMap(null);
+          marker=null;
+        }
+        if (marker == null) {
+          marker = new google.maps.Marker({
+            position: event.latLng,
+            map: map,
+            title: 'Your base',
+
+            draggable: true,
+            animation: google.maps.Animation.DROP
+          });
+          updateFieldsAndCenter(map, marker);
+          google.maps.event.addListener(marker, 'dragend', function() {
+            updateFieldsAndCenter(map, marker);
+          });
+        }
+      });
+    }
+
+    function updateMapAndCenter(map, marker) {
+
+      window.setTimeout(function() {
+        map.panTo(marker.getPosition());
+      }, 3000);
+    }
+    function updateFieldsAndCenter(map, marker) {
+
+      window.setTimeout(function() {
+        map.panTo(marker.getPosition());
+      }, 3000);
+
+      document.getElementById("_lat").value = marker.getPosition().lat();
+      document.getElementById("_lng").value = marker.getPosition().lng();
+    }
+    var image = 'assets/images/property.png';
+    var image2= 'assets/images/lokasi_sekitar.png';
+
+
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAz7xYV8gxtSq4_IE8aGv2L6Wx9vyzQtnc&callback=initMap">
+    </script> 
 
 <!-- <section class="featured-items-section">
   <div class="container">
@@ -581,90 +654,3 @@ src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAz7xYV8gxtSq4_IE8aGv2L6Wx
               </nav>
     </div>
   </div>
-
-
-        <script>
-      function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 10,
-          center: {lat: -6.204831, lng: 106.840848}
-        });
-
-        var geocoder = new google.maps.Geocoder();
-
-        document.getElementById('submit').addEventListener('click', function() {
-          geocodeAddress(geocoder, map);
-        });
-      }
-
-      function geocodeAddress(geocoder, resultsMap) {
-        var address = document.getElementById('address').value;
-        geocoder.geocode({'address': address}, function(results, status) {
-          if (status === google.maps.GeocoderStatus.OK) {
-            resultsMap.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-              map: resultsMap,
-              position: results[0].geometry.location
-            });
-            var lat = results[0].geometry.location.lat();
-            var lng = results[0].geometry.location.lng();
-          } else {
-            alert('Geocode was not successful for the following reason: ' + status);
-          }
-          document.getElementById('lat').value = lat;
-          document.getElementById('lng').value=lng;
-        });
-      }
-      var marker = null;
-    function initialize() {
-      var mapOptions = {
-        center: new google.maps.LatLng(-6.175486, 106.825407),
-        zoom: 15
-      };
-      var map = new google.maps.Map(document.getElementById("map-canvas"),
-          mapOptions);
-      google.maps.event.addListener(map, 'click', function(event) {
-        if (marker != undefined){
-          marker.setMap(null);
-          marker=null;
-        }
-        if (marker == null) {
-          marker = new google.maps.Marker({
-            position: event.latLng,
-            map: map,
-            title: 'Your base',
-
-            draggable: true,
-            animation: google.maps.Animation.DROP
-          });
-          updateFieldsAndCenter(map, marker);
-          google.maps.event.addListener(marker, 'dragend', function() {
-            updateFieldsAndCenter(map, marker);
-          });
-        }
-      });
-    }
-
-    function updateMapAndCenter(map, marker) {
-
-      window.setTimeout(function() {
-        map.panTo(marker.getPosition());
-      }, 3000);
-    }
-    function updateFieldsAndCenter(map, marker) {
-
-      window.setTimeout(function() {
-        map.panTo(marker.getPosition());
-      }, 3000);
-
-      document.getElementById("_lat").value = marker.getPosition().lat();
-      document.getElementById("_lng").value = marker.getPosition().lng();
-    }
-    var image = 'assets/images/property.png';
-    var image2= 'assets/images/lokasi_sekitar.png';
-
-
-    </script>
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAz7xYV8gxtSq4_IE8aGv2L6Wx9vyzQtnc&callback=initMap">
-    </script> -->
