942ba16c58b5aa181cd2137c1393a3f0dae5d4bd
amandaazallia <mandazal123@gmail.com>
2016-05-08 18:44:33 +0700

    update

--- application/views/find/pemilik/find.php
+++ application/views/find/pemilik/find.php
@@ -1,131 +1,31 @@
-<section class="content">
-<body class="footer-top-dark">
+  <img class="logo-maps" src="<?php echo base_url('assets/image/zuiplo.png');?>" alt="" width="150px;" height="50px;">
 
-<div id="page">
-    <div id="header-wrapper">
-        <div id="header">
-            <div id="header-inner  col-md-12">
-                <nav class="navbar navbar-default">
-                    <div class="header-secondary">
+    <div id="map">
 
-								    </div>
-
-                    <div class="container">
-                        <div class="navbar-header">
-
-
-                            <a class="navbar-brand" href="#">
-                                <span class="logo-styled">
-                                    <span class="logo-title">
-                                        <img class="logo-reg" src="<?php echo base_url('assets/image/zuiploo.png');?>" alt="" width="150px;" height="50px;">
-                                          <img class="logo-reg" src="<?php echo base_url('assets/image/zuiploo.png');?>" alt="" width="150px;" height="50px;">
-                                            <img class="logo-reg" src="<?php echo base_url('assets/image/zuiploo.png');?>" alt="" width="150px;" height="50px;">
-                                              <img class="logo-reg" src="<?php echo base_url('assets/image/zuiplo.png');?>" alt="" width="150px;" height="50px;">
-                                    </span>
-                                </span>
-                            </a>
-                        </div>
-
-                        <div class="collapse navbar-collapse" id="navbar-main">
-
-                    </div>
-                </nav>
-            </div>
-        </div>
     </div>
+    <!-- <div id="main-wrapper">
+        <div id="main">
+            <div id="main-inner"><div class="breadcrumb-wrapper">
+    <div class="container">
+        <ol class="breadcrumb pull-left">
 
+            <li><a href="#">
+              <form method="post"action="#">
+                <div class="form-group select-find">
+                  <select class="form-control option" name="#" id="#">
+                    <option value="All"></option>
+                    <option value="Near"></option>
+                    <option value="Far"></option>
+                  </select>
+                </div>
+              </form>
+            </a></li>
+            <li><a href="#">Proin era</a></li>
+        </ol>
 
-  </div>
-</div>
-</div>
-  <div class="map"></div>
+    </div>
+</div> -->
 
-        <script>
-      function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 10,
-          center: {lat: -6.204831, lng: 106.840848}
-        });
-
-        var geocoder = new google.maps.Geocoder();
-
-        document.getElementById('submit').addEventListener('click', function() {
-          geocodeAddress(geocoder, map);
-        });
-      }
-
-      function geocodeAddress(geocoder, resultsMap) {
-        var address = document.getElementById('address').value;
-        geocoder.geocode({'address': address}, function(results, status) {
-          if (status === google.maps.GeocoderStatus.OK) {
-            resultsMap.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-              map: resultsMap,
-              position: results[0].geometry.location
-            });
-            var lat = results[0].geometry.location.lat();
-            var lng = results[0].geometry.location.lng();
-          } else {
-            alert('Geocode was not successful for the following reason: ' + status);
-          }
-          document.getElementById('lat').value = lat;
-          document.getElementById('lng').value=lng;
-        });
-      }
-      var marker = null;
-    function initialize() {
-      var mapOptions = {
-        center: new google.maps.LatLng(-6.175486, 106.825407),
-        zoom: 15
-      };
-      var map = new google.maps.Map(document.getElementById("map-canvas"),
-          mapOptions);
-      google.maps.event.addListener(map, 'click', function(event) {
-        if (marker != undefined){
-          marker.setMap(null);
-          marker=null;
-        }
-        if (marker == null) {
-          marker = new google.maps.Marker({
-            position: event.latLng,
-            map: map,
-            title: 'Your base',
-
-            draggable: true,
-            animation: google.maps.Animation.DROP
-          });
-          updateFieldsAndCenter(map, marker);
-          google.maps.event.addListener(marker, 'dragend', function() {
-            updateFieldsAndCenter(map, marker);
-          });
-        }
-      });
-    }
-
-    function updateMapAndCenter(map, marker) {
-
-      window.setTimeout(function() {
-        map.panTo(marker.getPosition());
-      }, 3000);
-    }
-    function updateFieldsAndCenter(map, marker) {
-
-      window.setTimeout(function() {
-        map.panTo(marker.getPosition());
-      }, 3000);
-
-      document.getElementById("_lat").value = marker.getPosition().lat();
-      document.getElementById("_lng").value = marker.getPosition().lng();
-    }
-    var image = 'assets/images/property.png';
-    var image2= 'assets/images/lokasi_sekitar.png';
-
-
-    </script>
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAz7xYV8gxtSq4_IE8aGv2L6Wx9vyzQtnc&callback=initMap">
-    </script>
-<div id="map"> </div>
 <!-- <section class="featured-items-section">
   <div class="container">
     <div class="col-sm-12">
@@ -317,14 +217,14 @@
                   <li><a href="#">Lorem</a></li>
                   <li><a href="#">Lorem</a></li>
                   <li><a href="#">Lorem</a></li>
-              </ul>
+              </ul><!-- /.nav -->
           </nav>
 
           <div class="copyright">
             &copy;2016 Zuiplo. Created by JICOS
           </div>
-      </div>
-  </div>
+      </div><!-- /.container -->
+  </div><!-- /.footer-bottom --> -->
 
   <script>
 function initMap() {
@@ -633,3 +533,90 @@ src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAz7xYV8gxtSq4_IE8aGv2L6Wx
               </nav>
     </div>
   </div>
+
+
+        <script>
+      function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 10,
+          center: {lat: -6.204831, lng: 106.840848}
+        });
+
+        var geocoder = new google.maps.Geocoder();
+
+        document.getElementById('submit').addEventListener('click', function() {
+          geocodeAddress(geocoder, map);
+        });
+      }
+
+      function geocodeAddress(geocoder, resultsMap) {
+        var address = document.getElementById('address').value;
+        geocoder.geocode({'address': address}, function(results, status) {
+          if (status === google.maps.GeocoderStatus.OK) {
+            resultsMap.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+              map: resultsMap,
+              position: results[0].geometry.location
+            });
+            var lat = results[0].geometry.location.lat();
+            var lng = results[0].geometry.location.lng();
+          } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+          }
+          document.getElementById('lat').value = lat;
+          document.getElementById('lng').value=lng;
+        });
+      }
+      var marker = null;
+    function initialize() {
+      var mapOptions = {
+        center: new google.maps.LatLng(-6.175486, 106.825407),
+        zoom: 15
+      };
+      var map = new google.maps.Map(document.getElementById("map-canvas"),
+          mapOptions);
+      google.maps.event.addListener(map, 'click', function(event) {
+        if (marker != undefined){
+          marker.setMap(null);
+          marker=null;
+        }
+        if (marker == null) {
+          marker = new google.maps.Marker({
+            position: event.latLng,
+            map: map,
+            title: 'Your base',
+
+            draggable: true,
+            animation: google.maps.Animation.DROP
+          });
+          updateFieldsAndCenter(map, marker);
+          google.maps.event.addListener(marker, 'dragend', function() {
+            updateFieldsAndCenter(map, marker);
+          });
+        }
+      });
+    }
+
+    function updateMapAndCenter(map, marker) {
+
+      window.setTimeout(function() {
+        map.panTo(marker.getPosition());
+      }, 3000);
+    }
+    function updateFieldsAndCenter(map, marker) {
+
+      window.setTimeout(function() {
+        map.panTo(marker.getPosition());
+      }, 3000);
+
+      document.getElementById("_lat").value = marker.getPosition().lat();
+      document.getElementById("_lng").value = marker.getPosition().lng();
+    }
+    var image = 'assets/images/property.png';
+    var image2= 'assets/images/lokasi_sekitar.png';
+
+
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAz7xYV8gxtSq4_IE8aGv2L6Wx9vyzQtnc&callback=initMap">
+    </script> -->
