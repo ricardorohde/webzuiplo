ec3dbb029c2b46bd490216d92a216b6e307d4ffe
amandaazallia <mandazal123@gmail.com>
2016-05-08 13:44:34 +0700

    update design 4

--- application/views/dashboard/header.php
+++ application/views/dashboard/header.php
@@ -23,6 +23,8 @@
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/smint/css/style.css');?>">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/form-register.css');?>">
     <link href="<?php echo base_url('assets/css/bootstrap.min.css');?>" rel="stylesheet">
+    <link href="<?php echo base_url('assets/css/find/find.css');?>" rel="stylesheet">
+    <link href="<?php echo base_url('assets/css/find/style.css');?>" rel="stylesheet">
 
   	<!-- Import Line icons -->
   	<link href="<?php echo base_url('assets/css/simple-line-icons.css');?>" rel="stylesheet">
