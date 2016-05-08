f7e28576bd28e2e3e246b98d0e25389fed7590a4
amandaazallia <mandazal123@gmail.com>
2016-05-08 13:33:27 +0700

    update design 4

--- application/views/find/pemilik/find.php
+++ application/views/find/pemilik/find.php
@@ -1,10 +1,269 @@
+<body class="footer-top-dark">
 
+<div id="page">
+    <div id="header-wrapper">
+        <div id="header">
+            <div id="header-inner  col-md-12">
+                <nav class="navbar navbar-default">
+                    <div class="header-secondary">
+
+								    </div><!-- /.header-secondary -->
+
+                    <div class="container">
+                        <div class="navbar-header">
+
+
+                            <a class="navbar-brand" href="#">
+                                <span class="logo-styled">
+                                    <span class="logo-title">
+                                        <img class="logo-reg" src="<?php echo base_url('assets/image/zuiploo.png');?>" alt="" width="150px;" height="50px;">
+                                          <img class="logo-reg" src="<?php echo base_url('assets/image/zuiploo.png');?>" alt="" width="150px;" height="50px;">
+                                            <img class="logo-reg" src="<?php echo base_url('assets/image/zuiploo.png');?>" alt="" width="150px;" height="50px;">
+                                              <img class="logo-reg" src="<?php echo base_url('assets/image/zuiplo.png');?>" alt="" width="150px;" height="50px;">
+                                    </span><!-- /.logo-title -->
+                                    <span class="logo-subtitle hidden-sm">Real Estate <br/> Template</span>
+                                </span><!-- /.logo-styled -->
+                            </a>
+                        </div>
+
+                        <div class="collapse navbar-collapse" id="navbar-main">
+                      <!-- /.nav -->                        </div><!-- /.navbar-collapse -->
+                    </div>
+                </nav>
+            </div><!-- /#header-inner -->
+        </div><!-- /#header -->
+    </div><!-- /#header-wrapper -->
+
+    <div id="main-wrapper">
+        <div id="main">
+            <div id="main-inner"><div class="breadcrumb-wrapper">
+    <div class="container">
+        <ol class="breadcrumb pull-left">
+
+            <li><a href="#">
+              <form method="post"action="#">
+                <div class="form-group select-find">
+                  <select class="form-control option" name="#" id="#">
+                    <option value="All"></option>
+                    <option value="Near"></option>
+                    <option value="Far"></option>
+                  </select>
+                </div>
+              </form>
+            </a></li>
+            <li><a href="#">Proin era</a></li>
+        </ol><!-- /.breadcrumb -->
+
+    </div><!-- /.container -->
+</div><!-- /.breadcrumb-wrapper -->
+
+<section class="featured-items-section">
+  <div class="container">
+    <div class="col-sm-12">
+      <div class=" caption-slide-up">
+        <div class="col-lg-4 col-sm-6 col-md-6 featured-item">
+          <div class="labels">
+            <div class="label-rent"></div>
+            <div class="label-sale"></div>
+          </div>
+          <div class="category">
+            Home
+          </div>
+          <ul class="list-inline featured-item-header text-center">
+            <li><strong>
+              Price </strong><small>1.200.000 @ Month</small>
+            </li>
+          </ul>
+          <figure>
+            <img class="img-responsive" src="<?php echo base_url('assets/image/pictures/property-20150921052144.png');?>" alt="Amber Cottage">
+            <figcaption>
+              <h3>
+                Komp. Sqadron
+              </h3>
+              <p>
+                Jakarta                <br>
+                Halim PK                ,
+                81082 , 21 m<sup>2</sup>
+              </p>
+              <a href="#" class="button high_device_link"><span>&#10004;</span>View Detail</a>
+              <a href="#" class="small_device_link" >View Detail</a>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-lg-4 col-sm-6 col-md-6 featured-item">
+          <div class="labels">
+            <div class="label-rent"></div>
+            <div class="label-sale"></div>
+          </div>
+          <div class="category">
+            Home
+          </div>
+          <ul class="list-inline featured-item-header text-center">
+            <li><strong>
+              Price </strong><small>1.200.000 @ Month</small>
+            </li>
+          </ul>
+          <figure>
+            <img class="img-responsive" src="<?php echo base_url('assets/image/pictures/property-20150921052144.png');?>" alt="Amber Cottage">
+            <figcaption>
+              <h3>
+                Komp. Sqadron
+              </h3>
+              <p>
+                Jakarta                <br>
+                Halim PK                ,
+                81082 , 21 m<sup>2</sup>
+              </p>
+              <a href="#" class="button high_device_link"><span>&#10004;</span>View Detail</a>
+              <a href="#" class="small_device_link" >View Detail</a>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-lg-4 col-sm-6 col-md-6 featured-item">
+          <div class="labels">
+            <div class="label-rent"></div>
+            <div class="label-sale"></div>
+          </div>
+          <div class="category">
+            Home
+          </div>
+          <ul class="list-inline featured-item-header text-center">
+            <li><strong>
+              Price </strong><small>1.200.000 @ Month</small>
+            </li>
+          </ul>
+          <figure>
+            <img class="img-responsive" src="<?php echo base_url('assets/image/pictures/property-20150921052144.png');?>" alt="Amber Cottage">
+            <figcaption>
+              <h3>
+                Komp. Sqadron
+              </h3>
+              <p>
+                Jakarta                <br>
+                Halim PK                ,
+                81082 , 21 m<sup>2</sup>
+              </p>
+              <a href="#" class="button high_device_link"><span>&#10004;</span>View Detail</a>
+              <a href="#" class="small_device_link" >View Detail</a>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-lg-4 col-sm-6 col-md-6 featured-item">
+          <div class="labels">
+            <div class="label-rent"></div>
+            <div class="label-sale"></div>
+          </div>
+          <div class="category">
+            Home
+          </div>
+          <ul class="list-inline featured-item-header text-center">
+            <li><strong>
+              Price </strong><small>1.200.000 @ Month</small>
+            </li>
+          </ul>
+          <figure>
+            <img class="img-responsive" src="<?php echo base_url('assets/image/pictures/property-20150921052144.png');?>" alt="Amber Cottage">
+            <figcaption>
+              <h3>
+                Komp. Sqadron
+              </h3>
+              <p>
+                Jakarta                <br>
+                Halim PK                ,
+                81082 , 21 m<sup>2</sup>
+              </p>
+              <a href="#" class="button high_device_link"><span>&#10004;</span>View Detail</a>
+              <a href="#" class="small_device_link" >View Detail</a>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-lg-4 col-sm-6 col-md-6 featured-item">
+          <div class="labels">
+            <div class="label-rent"></div>
+            <div class="label-sale"></div>
+          </div>
+          <div class="category">
+            Home
+          </div>
+          <ul class="list-inline featured-item-header text-center">
+            <li><strong>
+              Price </strong><small>1.200.000 @ Month</small>
+            </li>
+          </ul>
+          <figure>
+            <img class="img-responsive" src="<?php echo base_url('assets/image/pictures/property-20150921052144.png');?>" alt="Amber Cottage">
+            <figcaption>
+              <h3>
+                Komp. Sqadron
+              </h3>
+              <p>
+                Jakarta                <br>
+                Halim PK                ,
+                81082 , 21 m<sup>2</sup>
+              </p>
+              <a href="#" class="button high_device_link"><span>&#10004;</span>View Detail</a>
+              <a href="#" class="small_device_link" >View Detail</a>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-lg-4 col-sm-6 col-md-6 featured-item">
+          <div class="labels">
+            <div class="label-rent"></div>
+            <div class="label-sale"></div>
+          </div>
+          <div class="category">
+            Home
+          </div>
+          <ul class="list-inline featured-item-header text-center">
+            <li><strong>
+              Price </strong><small>1.200.000 @ Month</small>
+            </li>
+          </ul>
+          <figure>
+            <img class="img-responsive" src="<?php echo base_url('assets/image/pictures/property-20150921052144.png');?>" alt="Amber Cottage">
+            <figcaption>
+              <h3>
+                Komp. Sqadron
+              </h3>
+              <p>
+                Jakarta                <br>
+                Halim PK                ,
+                81082 , 21 m<sup>2</sup>
+              </p>
+              <a href="#" class="button high_device_link"><span>&#10004;</span>View Detail</a>
+              <a href="#" class="small_device_link" >View Detail</a>
+            </figcaption>
+          </figure>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</section>
+  </div>
+  <div class="footer-bottom">
+      <div class="container">
+          <nav class="clearfix">
+              <ul class="nav navbar-nav footer-nav">
+                  <li><a href="#">Lorem</a></li>
+                  <li><a href="#">Lorem</a></li>
+                  <li><a href="#">Lorem</a></li>
+              </ul><!-- /.nav -->
+          </nav>
+
+          <div class="copyright">
+            &copy; Zuiplo. Created by JICOS
+          </div>
+      </div><!-- /.container -->
+  </div><!-- /.footer-bottom -->
+
+<!--
       <div id="map">
 
       </div>
       <div id="floating-panel" class="col-md-12 float">
         <div class="col-md-1"></div>
-          <!-- <div class="col-md-1"></div> -->
+
         <div class="col-md-6 logo-find">
           <div class="col-md-2">
             <img src="<?php echo base_url('assets/image/srikandev.png');?> ">
@@ -63,10 +322,10 @@
                                          <li><a href="#">Above</a></li>
 
                                        </ul>
-                                     </div><!-- /btn-group -->
-                                   </div><!-- /input-group -->
-                                 </div><!-- /.col-lg-6 -->
-                                </div><!-- /.row -->
+                                     </div>
+                                   </div>
+                                 </div>
+                                </div>
                           </div>
                           <div class="col-md-12 tab"> </div>
                                 <div class="col-md-12 tabs">
@@ -244,8 +503,8 @@
               map: resultsMap,
               position: results[0].geometry.location
             });
-            var lat = results[0].geometry.location.lat();// mendapatkan latitude
-            var lng = results[0].geometry.location.lng();// mendapatkan longitude
+            var lat = results[0].geometry.location.lat();
+            var lng = results[0].geometry.location.lng();
           } else {
             alert('Geocode was not successful for the following reason: ' + status);
           }
@@ -271,7 +530,7 @@
             position: event.latLng,
             map: map,
             title: 'Your base',
-            // Allowing to move the marker
+
             draggable: true,
             animation: google.maps.Animation.DROP
           });
@@ -282,19 +541,19 @@
         }
       });
     }
-    //==triger map jika ada perubahan lat - long
+
     function updateMapAndCenter(map, marker) {
-      // Center to marker position
+
       window.setTimeout(function() {
         map.panTo(marker.getPosition());
       }, 3000);
     }
     function updateFieldsAndCenter(map, marker) {
-      // Center to marker position
+
       window.setTimeout(function() {
         map.panTo(marker.getPosition());
       }, 3000);
-      // Change the value
+
       document.getElementById("_lat").value = marker.getPosition().lat();
       document.getElementById("_lng").value = marker.getPosition().lng();
     }
@@ -305,4 +564,4 @@
     </script>
     <script async defer
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAz7xYV8gxtSq4_IE8aGv2L6Wx9vyzQtnc&callback=initMap">
-    </script>
+    </script> -->
