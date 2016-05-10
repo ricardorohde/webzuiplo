3fe290bfc93f0ed065cc1fcd59a8106d8be9fe1b
amandaazallia <mandazal123@gmail.com>
2016-05-10 13:14:09 +0700

    update mock up 5

--- application/views/register/pemilik/add.php
+++ application/views/register/pemilik/add.php
@@ -8,7 +8,7 @@
 				<h2 class="title-reg">
 					<img src="<?php echo base_url('assets/image/zuiplo.png');?>" width="250px;" height="100px;" class="logo-head">
 				</h2>
-				<h2 class="title-text title-reg">
+				<h2 class="title-text">
 					Thank you for agreeing to take part in srikandev event planning survey!
 				</h2>
 			</div>
