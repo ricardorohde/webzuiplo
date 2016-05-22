0dd108aa19c57c2906eed24831757ea3386243b0
amandaazallia <mandazal123@gmail.com>
2016-05-22 20:42:19 +0700

    update final skip

--- application/views/register/pemilik/add.php
+++ application/views/register/pemilik/add.php
@@ -50,49 +50,11 @@
 
 
 <div class="container">
-	<div class="block-content">
-    	<div class="block-content-inner">
-			<div class="page-header page-header-no-rules center">
-				<h2 class=" title-reg">Register</h2>
-			</div>
-
-			<div class="row">
-    <div class="col-sm-4 col-sm-offset-4">
-        <form method="post" action="?">
-          <div class="form-group">
-              <label>Full Name</label>
-              <input type="text" class="form-control">
-          </div>
-
-            <div class="form-group">
-                <label>E-mail</label>
-                <input type="email" class="form-control">
-            </div>
-
-
-            <div class="form-group">
-                <label>Contact</label>
-                <input type="text" class="form-control">
-            </div>
-
-
-            <div class=" form-group">
-                <label>Address</label>
-                <textarea  class="form-control"></textarea>
-            </div>
-
-            <div class="form-group">
-                <input type="submit" value="Register" class="btn-lg btn-inversed-primary">
-            </div>
-        </form>
-    </div>
-</div>	    </div>
-
-<div class="container">
     <div>
         <div id="main">
             <div class="list-your-property-form">
-    <h2 class="page-header">List your property</h2>
+    <h2 class="page-header">Register your stopover
+</h2>
 
     <div class="content">
         <div class="row">
