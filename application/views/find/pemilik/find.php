d319ca74e26ba46ced756c1b1bf075ba8607868a
amandaazallia <mandazal123@gmail.com>
2016-05-22 20:03:28 +0700

    update final 5

--- application/views/find/pemilik/find.php
+++ application/views/find/pemilik/find.php
@@ -299,7 +299,20 @@
             </head>
             <body>
 
-    <div id="content">
+            <div class="container">
+            
+
+              <!-- Modal -->
+              <div class="modal fade" id="myModal" role="dialog">
+                <div class="modal-dialog modal-lg">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <button type="button" class="close" data-dismiss="modal">&times;</button>
+                      <h4 class="modal-title">Modal Header</h4>
+                    </div>
+                    <div class="modal-body">
+                      <p>  <!-- CONTENT -->
+            <div id="content">
 <div class="container">
     <div id="main">
         <div class="row">
