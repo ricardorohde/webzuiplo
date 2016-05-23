a25c98bb6f7029a8605265a0ce653b592ac5708b
amandaazallia <mandazal123@gmail.com>
2016-05-23 13:30:34 +0700

    update

--- application/views/dashboard/welcome.php
+++ application/views/dashboard/welcome.php
@@ -192,8 +192,9 @@
   .site-wrapper {
     padding-top: 0px;
     padding-bottom: 95px;
-    background-image: -webkit-linear-gradient(45deg, rgba(97, 0, 255, 0.49), rgba(151, 29, 252, 0.49) 22%, rgba(164, 65, 248, 0.49) 50%, rgba(90, 108, 239, 0.49) 72%, rgba(53, 130, 235, 0.49) 86%, rgba(30, 242, 241, 0.49)), url("<?php echo base_url('assets/image/bg.jpg');?>");
-    background-image: linear-gradient(45deg, rgba(97, 0, 255, 0.49), rgba(151, 29, 252, 0.49) 22%, rgba(164, 65, 248, 0.49) 50%, rgba(90, 108, 239, 0.49) 72%, rgba(53, 130, 235, 0.49) 86%, rgba(30, 242, 241, 0.49)), url("<?php echo base_url('assets/image/bg.jpg');?>");
+    background-image: -webkit-linear-gradient(45deg, rgba(16, 146, 38, 0.49), rgba(92, 138, 105, 0.49) 22%, rgba(93, 93, 93, 0.49) 50%, rgba(110, 204, 72, 0.49) 72%, rgba(35, 189, 29, 0.49) 86%, rgba(43, 230, 26, 0.49)), url("<?php echo base_url('assets/image/bg.jpg');?>");
+      background-image: linear-gradient(45deg, rgba(91, 173, 66, 0.49), rgba(108, 169, 97, 0.49) 22%, rgba(117, 158, 105, 0.49) 50%, rgba(40, 185, 66, 0.49) 72%, rgba(54, 160, 69, 0.49) 86%, rgba(30, 242, 241, 0.49)), url("<?php echo base_url('assets/image/bg.jpg');?>");
+
     background-position: 0% 0%, 50% 50%;
     background-size: auto, cover;
     background-attachment: scroll, fixed;
