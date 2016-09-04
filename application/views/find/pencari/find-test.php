58d7e9e1a777c22f63fd9f29e4de1a7da72e5ced
amandaazallia <mandazal123@gmail.com>
2016-09-04 20:53:41 +0700

    update

--- application/views/find/pencari/find-test.php
+++ application/views/find/pencari/find-test.php
@@ -312,62 +312,6 @@
                       <li><a href="#">last</a></li>
                   </ul>
               </div><!-- /.pagination -->
-              <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                   <!-- Indicators -->
-                   <ol class="carousel-indicators">
-                     <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                     <li data-target="#myCarousel" data-slide-to="1"></li>
-                     <li data-target="#myCarousel" data-slide-to="2"></li>
-                     <li data-target="#myCarousel" data-slide-to="3"></li>
-                   </ol>
-
-                   <!-- Wrapper for slides -->
-                   <div class="carousel-inner" role="listbox">
-
-                     <div class="item active">
-                       <img src="<?php echo base_url('assets/find/img/property.large.2.jpg');?>" alt="Chania" width="460" height="345">
-                       <div class="carousel-caption">
-                         <h3>Chania</h3>
-                         <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
-                       </div>
-                     </div>
-
-                     <div class="item">
-                        <img src="<?php echo base_url('assets/find/img/property.large.2.jpg');?>" alt="Chania" width="460" height="345">
-                       <div class="carousel-caption">
-                         <h3>Chania</h3>
-                         <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
-                       </div>
-                     </div>
-
-                     <div class="item">
-                     <img src="<?php echo base_url('assets/find/img/property.large.2.jpg');?>" alt="Chania" width="460" height="345">
-                       <div class="carousel-caption">
-                         <h3>Flowers</h3>
-                         <p>Beatiful flowers in Kolymbari, Crete.</p>
-                       </div>
-                     </div>
-
-                     <div class="item">
-                       <img src="<?php echo base_url('assets/find/img/property.large.2.jpg');?>" alt="Chania" width="460" height="345">
-                       <div class="carousel-caption">
-                         <h3>Flowers</h3>
-                         <p>Beatiful flowers in Kolymbari, Crete.</p>
-                       </div>
-                     </div>
-
-                   </div>
-
-                   <!-- Left and right controls -->
-                   <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-                     <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                     <span class="sr-only">Previous</span>
-                   </a>
-                   <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-                     <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                     <span class="sr-only">Next</span>
-                   </a>
-                 </div>
               <style>
                 .carousel-inner > .item > img,
                 .carousel-inner > .item > a > img {
